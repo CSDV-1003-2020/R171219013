5258538543440a33fe18bc5e890926c97749ae52
himanshu dubey <hd156681@gmail.com>
2020-03-30 17:31:54 +0530

    public private defined

---
+++ scm_project/sort/enqueue_dequeue.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class queue
+{
+private:
+    int rear,fron,capacity;
+    int *arr,choice;
+public:
+    queue(int y):capacity(y)
+    {
+        rear=fron=-1;
+        arr=new int[capacity];
+    }
+    int isemp()
+    {
+        return(fron==-1);
+    }
+    int isfull()
+    {
+        return(rear==capacity-1);
+    }
+    void enqueue();
+    void dequeue();
+    void menu();
+};
+void queue:: enqueue()
+    {
+        if(isfull())
+        {
+            cout<<"Queue is full:"<<endl;
+        }
+        else
+        {
+            rear++;
+            cout<<"Enter data:"<<endl;
+            cin>>arr[rear];
+            if(fron==-1)
+            {
+                fron=rear;
+            }
+        }
+    }
+void queue:: dequeue()
+int main()
