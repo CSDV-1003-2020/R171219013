50d0ed2852f83ae6146a529a3541476d36a4f6cd
AYUSH BANSAL <bayush611@gmail.com>
2020-04-07 12:40:41 +0530

    int main added'

--- scm_project/sort/merge_sort.cpp
+++ scm_project/sort/merge_sort.cpp
@@ -70,3 +70,27 @@ void mergeSort(int arr[], int l, int r)
         merge(arr, l, m, r);
     }
 }
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)  printf("%d ", A[i]);
+    printf("\n");
+}
+
+/* Driver program to test above functions */
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
+}
