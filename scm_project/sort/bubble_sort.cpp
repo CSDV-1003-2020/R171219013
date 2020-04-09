eb78c054129058d4c0644806413e6757e17136ad
himanshu dubey <hd156681@gmail.com>
2020-04-09 18:25:38 +0530

    code completed

--- scm_project/sort/bubble_sort.cpp
+++ scm_project/sort/bubble_sort.cpp
@@ -17,5 +17,25 @@ void bubbleSort(int arr[], int n)
     for (j = 0; j < n-i-1; j++)  
         if (arr[j] > arr[j+1])  
             swap(&arr[j], &arr[j+1]);  
-}  
+} 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver code
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+   cout<<"Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+//bubblesort
   
