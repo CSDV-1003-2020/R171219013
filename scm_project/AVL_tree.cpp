0c49b641aea51483b94a3db31864fa240d340aaf
himanshu dubey <hd156681@gmail.com>
2020-04-16 02:22:39 +0530

    insert added

--- scm_project/AVL_tree.cpp
+++ scm_project/AVL_tree.cpp
@@ -90,3 +90,18 @@ avl *avl_tree::balance(avl *t) {
    }
    return t;
 }
+avl *avl_tree::insert(avl *r, int v) {
+   if (r == NULL) {
+      r = new avl;
+      r->d = v;
+      r->l = NULL;
+      r->r = NULL;
+      return r;
+   } else if (v< r->d) {
+      r->l = insert(r->l, v);
+      r = balance(r);
+   } else if (v >= r->d) {
+      r->r = insert(r->r, v);
+      r = balance(r);
+   } return r;
+}
